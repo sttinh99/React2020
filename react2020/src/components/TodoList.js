b40565d7d24665394cdcb859b0737af612bafce0
sttinh99 <17110381@student.hcmute.edu.vn>
2020-08-26 21:53:28 +0700

    check job complete

--- react2020/src/components/TodoList.js
+++ react2020/src/components/TodoList.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import classNames from "classnames";
 import "./TodoList.css";
 
+import tick from "../images/tick.svg";
+import check from "../images/check.svg";
+
 class TodoList extends Component {
   //   constructor(props) {
   //     super(props);
@@ -12,11 +15,16 @@ class TodoList extends Component {
   //   }
   render() {
     const { item, onClick } = this.props;
+    let url = tick;
+    if (!item.isComplete) {
+      url = check;
+    }
     return (
       <div
         onClick={onClick}
         className={classNames("TodoItem", { complete: item.isComplete })}
       >
+        <img src={url} alt="picture" />
         <p>{item.title}</p>
       </div>
     );
